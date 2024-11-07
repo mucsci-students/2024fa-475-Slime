04d30a1ec17d1c195d74eba7881d67e228e4892b
Kevin Dichter <kevinidichter@gmail.com>
2024-11-07 18:51:27 -0500

    improved snow particle position

--- Game2/Assets/Scripts/SnowParticles.cs
+++ Game2/Assets/Scripts/SnowParticles.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SnowParticles : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class SnowParticles : MonoBehaviour
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.tag == "PlayerBox")
-        currentParticles = Instantiate(particleSpawn, playerScript.transform.position, particleSpawn.transform.rotation);
+        currentParticles = Instantiate(particleSpawn, new Vector3(playerScript.transform.position.x, playerScript.transform.position.y - .25f, 0), particleSpawn.transform.rotation);
         StartCoroutine(destroyParticles());
     }
 
