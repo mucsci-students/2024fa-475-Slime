8edf188e44c88542690d96b0f94093b7ef8563d7
Kevin Dichter <kevinidichter@gmail.com>
2024-11-11 16:09:04 -0500

    got music to swap between scenes

---
+++ Game2/Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "PlayerBox")
+        {
+            audioSource.Play();
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.tag == "PlayerBox")
+        {
+            audioSource.Pause();
+        }
+    }
+}
